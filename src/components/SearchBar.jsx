0f15b8c4127bdbddfd7bd4424e6c19715bbf073e
vincentt14 <vincent14@student.umn.ac.id>
2022-12-05 16:12:12 +0700

    fixing minor bugs & vercel deploy
    fix & deploy using vercel

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form className="ui form" onSubmit={onSubmit}>
         <div className="field">
-          <label>Video Search</label>
+          <label>Search For A Video</label>
           <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
         </div>
       </form>
